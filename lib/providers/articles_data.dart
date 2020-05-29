9e153af0c44ce7f5ee6b513a48b4ed73d5704073
Coezette Sibanda <cs3353@nyu.edu>
2020-05-29 21:35:34 +0400

    Added location/local news functionality. Ready for first version deployment. Still to add social functionality

--- lib/providers/articles_data.dart
+++ lib/providers/articles_data.dart
@@ -50,11 +50,11 @@ class TrendingCategoryNews {
 
 //Local News
 class LocalNews {
-  static List<ArticleModel> articles = new List<ArticleModel>();
+  List<ArticleModel> articles = new List<ArticleModel>();
 
-  static Future<void> getNews(String location) async {
+  Future<void> getNews(String location) async {
     String url =
-        "https://newsapi.org/v2/top-headlines?country=za&apiKey=$NEWS_API_KEY&pageSize=100";
+        "https://newsapi.org/v2/top-headlines?country=$location&apiKey=$NEWS_API_KEY&pageSize=100";
 
     var response = await http.get(url); //just gets the response as a string
     var jsonData = jsonDecode(
