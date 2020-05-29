fa9cd911ba32b8ca86e746ec4e7bbd65d63ebdd3
Coezette Sibanda <cs3353@nyu.edu>
2020-05-29 21:35:34 +0400

    Added location/local news functionality. Ready for first version deployment. Still to add social functionality

--- lib/screens/main_bodies/local_news_screen.dart
+++ lib/screens/main_bodies/local_news_screen.dart
@@ -6,6 +6,7 @@ import 'package:incite/providers/articles_data.dart';
 import 'package:incite/providers/categories_data.dart';
 import 'package:incite/widgets/articles_list.dart';
 import 'package:incite/widgets/category_tile.dart';
+import 'package:incite/providers/locations.dart';
 
 var categories = new List<CategoryModel>();
 List<ArticleModel> trendingLocalArticles = new List<ArticleModel>();
@@ -15,7 +16,7 @@ class LocalNewsScreen extends StatefulWidget {
 
   String location;
 
-  LocalNewsScreen({this.location = 'us'});
+  LocalNewsScreen({this.location = 'za'});
 
   @override
   _LocalNewsScreenState createState() => _LocalNewsScreenState();
@@ -27,15 +28,29 @@ class _LocalNewsScreenState extends State<LocalNewsScreen> {
   void initState() {
     super.initState();
     categories = CategoriesData.getCategories();
-    getNews();
+    _getNews(widget.location);
   }
 
-  void getNews() async {
-    await LocalNews.getNews(widget.location);
-    trendingLocalArticles = LocalNews.articles;
+  void _getNews(String loc) async {
+    var localNewsObject = LocalNews();
+    await localNewsObject.getNews(loc);
+    trendingLocalArticles = localNewsObject.articles;
     setState(() {
       _isLoading = false;
     });
+  }
+
+  List<DropdownMenuItem> getDropdownItems() {
+    List<DropdownMenuItem<String>> dropdownItems = [];
+
+    Locations.newsLocations.forEach((key, value) {
+      var countryItem = DropdownMenuItem(
+        child: Text(key),
+        value: value,
+      );
+      dropdownItems.add(countryItem);
+    });
+    return dropdownItems;
   }
 
   @override
@@ -48,6 +63,33 @@ class _LocalNewsScreenState extends State<LocalNewsScreen> {
             padding: const EdgeInsets.symmetric(horizontal: 10),
             child: Column(
               children: <Widget>[
+                SizedBox(
+                  height: 40,
+                  child: Container(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            "Location:",
+                          ),
+                          DropdownButton(
+                            style: TextStyle(color: Colors.blue, fontSize: 16),
+                            value: widget.location,
+                            items: getDropdownItems(),
+                            onChanged: (value) {
+                              setState(() {
+                                widget.location = value;
+                              });
+                              _getNews(value);
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
                 Divider(thickness: 1),
                 SizedBox(
                   height: 40,
